1918a06da64f96ef42242f8eb4c2a5771a6f1ee6
David zhang <d.ydzdavid@yahoo.com>
2019-04-03 21:39:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,7 +36,11 @@ class Song
   end 
   
   def self.artist_count
-    
+    artist_hash = {}
+    @@artists.uniq.each do |art|
+      artist_hash[art] = @@genres.count(art)
+    end
+    artist_hash
   end
   
 end   
