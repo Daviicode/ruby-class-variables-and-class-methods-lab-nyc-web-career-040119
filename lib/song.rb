2997c175a46783969c2f006f17781adaeb4aed44
David zhang <d.ydzdavid@yahoo.com>
2019-04-03 20:46:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,8 +2,7 @@ class Song
   attr_accessor :name, :artist, :genre
   
   @@count = 0
-  @@artist = []
-  @@genres = []
+  
   
   def initialize(name, artist, genre)
     @name = name
@@ -11,6 +10,8 @@ class Song
     @genre = genre
     
     @@count += 1 
+    @@artist = []
+    @@genres = []
   end
   
   
